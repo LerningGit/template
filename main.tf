dc4cbf07ac01f5328738b4b8b82be654c336655f
LearningGit <109271563+LerningGit@users.noreply.github.com>
2023-05-31 10:06:22 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = ">= 3.0"
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
+  monitoring = true
+
+  metadata_options {
+    http_tokens = "required"
+  }
+  
+  root_block_device {
+    encrypted = true
+  }
+  
+  ebs_optimized = true
+  
+  iam_instance_profile = "your-iam-role"
+}
